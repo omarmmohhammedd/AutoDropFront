cbc3a4b64263a06643565f45eac4bd6728c88ef1
Omar Mohammed <63738680+omarmmohhammedd@users.noreply.github.com>
2023-11-09 02:29:45 +0200

    dit

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -119,7 +119,7 @@ export default function Dashboard() {
           if (value) {
             return {
               ...card,
-              value: card.type === 'currency' ?   Number(value)  % 1 !== 0 ?CurrencyFormatter(Number(Number(value).toFixed(3)) || 0)  :CurrencyFormatter(value || 0)   : value
+              value: card.type === 'currency' ?   Number(value)  % 1 !== 0 ?CurrencyFormatter(Number(Number(value).toFixed(3)) || 0)  :CurrencyFormatter(value || 0)   :  Number(value)  % 1 !== 0  ? Number(value).toFixed(3):value
             };
           }
           return card;
